5c4588ce6831194729e346b6a4cba2961e141ddb
Bob Long <rlong@unicon.net>
2026-09-24 12:40:14 -0400

    Log the created experiment's ID after creation instead of its (usually null) title before

--- src/main/java/edu/iu/terracotta/controller/app/ExperimentController.java
+++ src/main/java/edu/iu/terracotta/controller/app/ExperimentController.java
@@ -112,7 +112,6 @@ public class ExperimentController {
             experimentDto = ExperimentDto.builder().build();
         }
 
-        log.debug("Creating Experiment with title : {}", experimentDto.getTitle());
         SecuredInfo securedInfo = apijwtService.extractValues(req,false);
 
         if (securedInfo == null) {
@@ -138,6 +137,7 @@ public class ExperimentController {
         }
 
         ExperimentDto returnedDto = experimentService.postExperiment(experimentDto, securedInfo);
+        log.debug("Created Experiment ID : [{}]", returnedDto.getExperimentId());
         HttpHeaders headers = experimentService.buildHeaders(ucBuilder, returnedDto.getExperimentId());
 
         return new ResponseEntity<>(returnedDto, headers, HttpStatus.CREATED);
